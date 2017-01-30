a8244dbefc21c9820821d252f0c4c2a1d7589530
edgararuiz <edgararuiz@gmail.com>
2017-01-29 18:45:22 -0600

    moved credentials to an ignored script

--- sampler_server.R
+++ sampler_server.R
@@ -1,6 +1,3 @@
-cache_credentials(client_id = "1085813435165-epc453ido1lo0amaiu9v9o78l9evq50t.apps.googleusercontent.com",
-                  client_secret = "aQMrJBuVkvHMBnDeNB__M5ws",
-                  redirect_uri = "http://localhost:1410/new")
 
 
 library(plumber)
